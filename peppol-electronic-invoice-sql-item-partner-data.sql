da8b248ee47288e42f8acb350776f635d6a4a966
Yurino Oga <yurino.o@latona.io>
2022-10-17 19:09:59 +0900

    release

--- peppol-electronic-invoice-sql-item-partner-data.sql
+++ peppol-electronic-invoice-sql-item-partner-data.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `peppol_electronic_invoice_item_partner_data`
 (
     `ID`                          varchar(10) NOT NULL,      -- <cbc> "F012345"
-    `InvoiceLineID`               varchar(10) NOT NULL,      -- <cbc/cac:InvoiceLine> "1"
+    `InvoiceLineID`               varchar(6) NOT NULL,      -- <cbc/cac:InvoiceLine> "1"
     `PartnerFunction`             varchar(2) NOT NULL,       -- <na> "BL" Partner Function にマッピング済
     `PartnerID`                   varchar(10) NOT NULL,      -- <cbc> "INV0001"
     `PartnerName`                 varchar(100) DEFAULT NULL, -- <cbc> "IMDA1-Infocomm Media Devt Authority"
